8a90ff954e012cefb3bd4a73d56f2a35ecf549e6
alum.fial7 <alum.fial7@FIA-LAB7B3.lim.upeu.edu.pe>
2014-06-25 15:27:41 -0500

    Version3:Desarrollando clases 2

--- src/pe/upeu/edu/pe/DAO/DistritoDAO.java
+++ src/pe/upeu/edu/pe/DAO/DistritoDAO.java
@@ -6,10 +6,45 @@
 
 package pe.upeu.edu.pe.DAO;
 
+import java.sql.Statement;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import pe.upeu.edu.pe.config.Conexion;
+import pe.upeu.edu.pe.modelo.Distrito;
+
 /**
  *
  * @author alum.fial7
  */
 public class DistritoDAO {
+    private Connection cx;
+    private Statement st;
+    private ResultSet rs;
+    private String sql;
     
+    ArrayList<Distrito> lista = null;
+    public ArrayList<Distrito> listarDistrito(){
+        lista = new ArrayList();
+        sql="SELECT * FORM distrito";
+        try{
+            cx=Conexion.getConex();
+            st=cx.createStatement();
+            rs=st.executeQuery(sql);
+            while(rs.next()){
+                Distrito d=new Distrito();
+                d.setIdd(rs.getInt("iddistrito"));
+                d.setIdr(rs.getInt("iddregion"));
+                d.setDisc(rs.getString("distrito"));
+                d.setEstado(rs.getString("estado"));
+                lista.add(d);
+            }
+            
+        }catch(SQLException e){
+            
+        }
+        
+        return lista;
+    }
 }
--- src/pe/upeu/edu/pe/DAO/TipoIglesiaDAO.java
+++ src/pe/upeu/edu/pe/DAO/TipoIglesiaDAO.java
@@ -6,10 +6,44 @@
 
 package pe.upeu.edu.pe.DAO;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import pe.upeu.edu.pe.config.Conexion;
+import pe.upeu.edu.pe.modelo.Distrito;
+import pe.upeu.edu.pe.modelo.TipoIglesia;
+
 /**
  *
  * @author alum.fial7
  */
 public class TipoIglesiaDAO {
+    private Connection cx;
+    private Statement st;
+    private ResultSet rs;
+    private String sql;
     
+    ArrayList<TipoIglesia> lista = null;
+    public ArrayList<TipoIglesia> listarIglesia(){
+        lista = new ArrayList();
+        sql="SELECT * FORM tipo_iglesia";
+        try{
+            cx=Conexion.getConex();
+            st=cx.createStatement();
+            rs=st.executeQuery(sql);
+            while(rs.next()){
+                TipoIglesia ti=new TipoIglesia();
+                ti.setIdti(rs.getInt("idtipo_iglesia"));
+                ti.setNomtipo(rs.getString("tipo_iglesia"));
+                lista.add(ti);
+            }
+            
+        }catch(SQLException e){
+            
+        }
+        
+        return lista;
+    }
 }
